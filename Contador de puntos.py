2eb4741a70a6cc3288eceb78cf825f3d78157bd5
Rocio.G <146854769+Rocio466@users.noreply.github.com>
2023-11-14 19:17:37 -0300

    Add files via upload
    Se hizo cambios en el código y se creó una función encapsulando todo el código.

--- Contador de puntos.py
+++ Contador de puntos.py
@@ -1,43 +1,27 @@
-class ContadorDePuntos:
-    def __init__(self):
-        self.puntos = 0
-        print("Puntos iniciales:", self.obtener_puntos())
+def crear_contador_de_puntos():
+    class ContadorDePuntos:
+        def __init__(self):
+            self.puntos = 0
+            self.ultimas_letras = []  # Lista para guardar las últimas letras ingresadas
+            print("Puntos iniciales:", self.obtener_puntos())
 
-    def sumar_puntos(self, cantidad):
-        self.puntos += cantidad
-        print("Puntos después de sumar 1:", self.obtener_puntos())
+        def sumar_puntos(self, cantidad):
+            self.puntos += cantidad
+            print("Puntos después de sumar:", self.obtener_puntos())
 
-    def restar_puntos(self, cantidad):
-        self.puntos -= cantidad
-        print("Puntos después de restar 1:", self.obtener_puntos())
+            # Verificar si las últimas tres letras son 'x'
+            self.ultimas_letras.append('x')
+            if len(self.ultimas_letras) >= 3 and all(letra == 'x' for letra in self.ultimas_letras[-3:]):
+                self.puntos += 1
+                print("¡Se sumó 1 punto por tener tres 'x' seguidas!")
 
-    def reiniciar_puntos(self):
-        self.puntos = 0
-        print("Puntos después de reiniciar:", self.obtener_puntos())
+        def reiniciar_puntos(self):
+            self.puntos = 0
+            print("Puntos después de reiniciar:", self.obtener_puntos())
 
-    def obtener_puntos(self):
-        return self.puntos
-
-contador = ContadorDePuntos()
-
-while True:
-    print("\n1. Sumar puntos")
-    print("2. Restar puntos")
-    print("3. Reiniciar puntos")
-    print("4. Salir")
+        def obtener_puntos(self):
+            return self.puntos
     
-    opcion = input("Seleccione una opción (1-4): ")
-    
-    if opcion == '1':
-        cantidad = int(input("Ingrese la cantidad de puntos a sumar: "))
-        contador.sumar_puntos(cantidad)
-    elif opcion == '2':
-        cantidad = int(input("Ingrese la cantidad de puntos a restar: "))
-        contador.restar_puntos(cantidad)
-    elif opcion == '3':
-        contador.reiniciar_puntos()
-    elif opcion == '4':
-        print("Saliendo del programa.")
-        break
-    else:
-        print("Opción no válida. Por favor, seleccione una opción válida.")
+    return ContadorDePuntos()
+
+
